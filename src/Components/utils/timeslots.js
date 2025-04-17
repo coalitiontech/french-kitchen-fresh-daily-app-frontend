d0fa8bf59dd0d5bf312573e986f5ba0cbfc1262d
Arlon Scheidegger <arlon.scheidegger@coalitiontechnologies.com>
2025-04-17 18:14:54 -0300

    fixing time slots

--- src/Components/utils/timeslots.js
+++ src/Components/utils/timeslots.js
@@ -1,7 +1,7 @@
 export function generateTimeslots() {
     const timeslots = [];
-    for (let hour = 0; hour < 24; hour++) {
-        for (let minute = 0; minute < 60; minute += 15) {
+    for (let hour = 7; hour <= 18; hour++) {
+        for (let minute = 0; minute < 60; minute += 60) {
             const time = `${String(hour).padStart(2, '0')}:${String(minute).padStart(2, '0')}`;
             timeslots.push(time);
         }
